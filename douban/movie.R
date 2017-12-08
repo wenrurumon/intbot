bc09d0aa9aee63f9688ceb7343c32341a8501057
進撃の雯雯老師 <askahu6@gmail.com>
2017-12-08 22:52:47 +0800

    Update movie.R

--- douban/movie.R
+++ douban/movie.R
@@ -34,6 +34,33 @@ chrome$open()
 # url <- unlist(chrome$getCurrentUrl())
 
 url_base <- 'https://movie.douban.com/people/'
-id <- 64077663
+id <- 71066593
 url <- paste0(url_base,id,'/')
-chrome$navigate(url)
+labs <- c('collect','wish','do')
+
+getmovie <- function(i,lab='collect',getp=F){
+  # print(i)
+  i <- (i-1)*15
+  urli <- paste0(url,lab,'?start=',i)
+  chrome$navigate(urli)
+  p <- try(as.numeric(strsplit(geteletext(chrome$findElement('class','paginator'))[[1]],'[^0-9]')[[1]]))
+  if(class(p)=="try-error"){p <- 1}
+  if(getp){return(max(p,na.rm=T))}
+  items <- chrome$findElements('class','item')
+  geteletext(items)
+}
+p <- sapply(labs,getmovie,i=1,getp=T)
+getmovies <- function(p){
+  lapply(1:3,function(j){
+    outj <- do.call(c,do.call(c,lapply(1:p[j],getmovie,names(p)[j],getp=F)))
+    outj
+  })
+}
+out <- getmovies(p)
+rlt <- lapply(out,function(x){
+  do.call(rbind,lapply(strsplit(x,'\n'),function(x){
+    if(length(x)==3){x <- c(x,NA)}
+    return(x)
+  }))
+})
+substr(rlt[[1]][,1:3],1,10)
