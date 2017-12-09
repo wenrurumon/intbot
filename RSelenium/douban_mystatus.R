716af860f291bd10c783e97807522cf85ddf7b06
進撃の雯雯老師 <askahu6@gmail.com>
2017-12-09 18:40:10 +0800

    Update douban_mystatus.R

--- RSelenium/douban_mystatus.R
+++ RSelenium/douban_mystatus.R
@@ -29,52 +29,31 @@ chrome$open()
 ###########################
 
 #check guangbo
-# url <- 'https://www.douban.com/mine/'
-# chrome$navigate(url)
-# url <- unlist(chrome$getCurrentUrl())
+url <- 'https://www.douban.com/mine/'
+chrome$navigate(url)
+url <- unlist(chrome$getCurrentUrl())
 
-url_base <- 'https://www.douban.com/people/'
-id <- 64077663
-url <- paste0(url_base,id,'/')
+# url_base <- 'https://www.douban.com/people/'
+# id <- 64077663
+# url <- paste0(url_base,id,'/')
 
 getguangbo <- function(i){
-  print(i)
   urli <- paste0(url,'statuses?p=',i)
   chrome$navigate(urli)
-  items <- chrome$findElements('class','stream-items')
+  # items <- chrome$findElements('class','status-item')
+  items <- chrome$findElements('class','new-status')
   items <- unlist(geteletext(items))
-  items <- strsplit(items,'\n')[[1]]
+  p <<- length(items)
+  print(paste(i,p))
   items
 }
-myguangbo <- do.call(c,lapply(1:3,getguangbo))
-myguangbo <- sapply(strsplit(myguangbo,' '),function(x){x[[1]]})
-id <- names(sort(-table(myguangbo)))[1]
-
-getguangbo2 <- function(i){
-  print(i)
-  urli <- paste0(url,'statuses?p=',i)
-  chrome$navigate(urli)
-  items <- chrome$findElements('class','stream-items')
-  items <- unlist(geteletext(items))
-  items <- strsplit(items,'\n')[[1]]
-  start <- which(substr(items,1,nchar(id)+1) == paste0(id,' '))
-  end <- c(start[-1]-1,length(items))
-  lapply(1:length(start),function(j){
-    items[start[j]:end[j]]
-  })
+rlt <- list()
+p <- 20
+i <- 1
+while(p>0){
+ rlt[[i]] <- getguangbo(i)
+ i <- i+1
 }
+rlt <- do.call(c,rlt)
+h <- sapply(strsplit(rlt,'\n'),function(x){x[1]})
 
-n <- 9
-myguangbo <- lapply(1:n,getguangbo2)
-myguangbo <- do.call(c,myguangbo)
-# save(myguangbo,file='myguangbo.rda')
-# load('myguangbo.rda')
-myguangbo <- lapply(myguangbo,function(x){
-  x <- gsub('  ',' ',x)
-})
-
-h <- substr(sapply(myguangbo,function(x){x[[1]]}),1,nchar(id)+3)
-myguangbo <- lapply(unique(h),function(x){
-  myguangbo[h==x]
-})
-myguangbo[grep('看',unique(h))]
