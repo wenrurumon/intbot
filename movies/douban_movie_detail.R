1b369625b03e60af0fda0fe68762ec0acfe7ab44
進撃の雯雯老師 <askahu6@gmail.com>
2018-01-15 10:21:40 +0800

    Update douban_movie_detail.R

--- movies/douban_movie_detail.R
+++ movies/douban_movie_detail.R
@@ -51,9 +51,11 @@ get_movie_url <- function(keyword){
 
 get_movie_info <- function(url){
   chrome$navigate(url)
+  print('navigate url for movie')
   info <- strsplit(geteletext(chrome$findElement('id','info'))[[1]],'\n')
   intro <- geteletext(chrome$findElement('id','link-report'))[[1]]
   #short comments
+  print('get short comment for movie')
   cmts <- lapply(0:4,function(i){
     shorts <- paste0(url,'comments?start=',i*20,'&limit=20&sort=new_score&status=P&percent_type=')
     chrome$navigate(shorts)
@@ -67,13 +69,15 @@ get_movie_info <- function(url){
   })
   cmts <- do.call(rbind,cmts)
   #long comments
+  print('get long comment for movie')
   chrome$navigate(paste0(url,'reviews?sort=hotest'))
   lurl <- chrome$findElements('tag name','a')
   lurl <- unlist(geteleattr(lurl,attr='href'))
   lurl <- lurl[grep('#comment',lurl)]
+  lurl <- gsub('#comments','',lurl)
   lcmts <- lapply(lurl,function(li){
     chrome$navigate(li)
-    geteletext(chrome$findElement('id','link-report'))[[1]]
+    try(geteletext(chrome$findElement('id','link-report'))[[1]])
   })
   #output
   rlt <- list(url=url,info=info,intro=intro,shortcomment=cmts,longcomment=lcmts)
@@ -87,9 +91,44 @@ model <- function(x){
   out
 }
 
+model2 <- function(x){try(model(x))}
+
 #########################
 # Test
 #########################
 
-movies <- c('虎妈猫爸','芳华','妖猫传')
+movies <- unlist(strsplit(
+'迷雾
+寂静岭
+孤儿怨
+第六感
+闪灵
+妖猫传
+芳华
+大护法
+鲛珠传
+看不见的客人
+喜剧之王
+楚门的世界
+一天
+火星救援
+天使爱美丽
+朗读者
+后会无期
+了不起的盖茨比
+岁月神偷
+模仿游戏
+唐山大地震
+卡萨布兰卡
+阳光小美女
+催眠大师
+比利林恩的中场战事
+海边的曼彻斯特
+无人知晓
+请以你的名字呼唤我
+辩护人
+假如爱有天意
+'
+  ,'\n'
+))
 out <- lapply(movies,model)
